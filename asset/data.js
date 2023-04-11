629c53f447d183ac10b0402d13b8b7fb2865d5a6
patriciaa-t <124017428+patriciaa-t@users.noreply.github.com>
2023-04-11 10:52:14 +0700

    Add files via upload

---
+++ asset/data.js
@@ -0,0 +1,265 @@
+var articles = [
+
+//Amphibians
+{
+    title: "Restore linked habitat to protect tropical amphibians from disease: Study",
+    foto: "foto art 1.jpg",
+    code: 1,
+    category: "Amphibians",
+    date: 29-02-23,
+    views: 4245,
+    author: "Natanaele Bharat Elyj",
+    content: "Article:As a precipitous global biodiversity decline escalates, forests across the tropics are increasingly becoming fragmented. This fragmentation can lead to multiple problems, including a process known as “habitat split” that could have consequences for disease among amphibians, according to a recently published study in the journal Biological Reviews. Habitat split occurs when multiple “classes” of habitat — such as forests, streams and ponds — that are important to some amphibian species’ life cycles are separated, Gui Becker, first author on the paper and a biologist with Pennsylvania State University, told Mongabay in an interview.In earlier work, Becker and colleagues concluded that habitat split was a driving force behind local extinctions of amphibians in South America’s highly fragmented Atlantic Forest. However, a link between habitat split and the role of disease, had not then been considered.Of particular concern when it comes to amphibian disease is the chytrid fungus, Batrachochytrium dendrobatidis, which is linked to population declines in Brazil and to amphibian species losses across the globe. Also known as Bd, this fungus can cause the potentially fatal disease chytridiomycosis, with scientists already knowing prior to this research that habitat fragmentation can increase the risk of Bd infections among amphibians."
+},
+
+{
+    title: "The 8 Best Pet Frogs For Beginners",
+    foto: "foto art 2.jpg",
+    code: 2,
+    category: "Amphibians",
+    date: 28-02-23,
+    views: 4654,
+    author: "Luciano Mars",
+    content: "The Amazon milk frog (Trachycephalus resinifictrix) is a well-liked species of frog among herp keepers. The Amazon milk frog is an arboreal species that typically hunt from perches in the tree tops near wooded areas. It is commonly raised in captivity and naturally reproduces in tree holes. Males’ sizes start at 2.5 inches and go up to 4 inches, while women’s sizes start at 2 inches. Their huge size, large hands, and large toe pads all help them in their tree-dwelling lifestyle. One of the largest native frogs in the Americas is the American bullfrog (Rana catesbeiana). The American bullfrog is a huge, powerful, and brutish frog that can reach a maximum length of about 8 inches. With their large, webbed feet, these frogs are excellent swimmers, and their ravenous appetites mean they can devour virtually any food item that fits in their mouths. The frog is an invasive species in many areas, and it can be found naturally in 36 U.S. states. The American bullfrog has been successfully bred in captivity, and its tadpoles may be obtained at most aquariums, pet stores, and reptile expos. The Argentine (or ornate, or Bell’s) horned frog (Ceratophrys ornata) is affectionately known as the Pac-Man frog due in part to the size of its mouth and the way in which it stuffs prey items into it. The frog is commonly kept in captivity, reproduced, and comes in a broad variety of morphs, making it a favorite among herp keepers. These amphibians reach a maximum size of around 6 inches, and they are not particularly energetic creatures, rather instead to wait patiently for prey to come to them.",
+},
+
+{
+    title: "Global pet trade in amphibians is bigger than we thought",
+    foto: "foto art 3.jpg",
+    code: 3,
+    category: "Amphibians",
+    date: 27-02-23,
+    views: 8745,
+    author: "Silvestra Mariana",
+    content: "Humans keep all sorts of animals as pets and new species are constantly being brought into the trade – some reared in captivity, but many sourced from the wild. As a result, the global pet trade often puts wild populations at risk of over-exploitation. The Global Amphibian Assessment pegs 47 amphibian species to be predominantly threatened by unsustainable harvesting for the international pet trade. And the risks don’t end there. For various reasons, people release pets into the wild, resulting in biological invasions. People might do this because keeping the animal isn’t the experience they expected, or because they can’t afford it any longer. The problem is that, often, it may introduce a species to regions beyond their natural range. These invasive populations can harm native species and lead to the spread of diseases to new areas. For example, the pet trade is linked to the spread of an infectious fungal disease of salamanders in Europe, leading to large scale salamander mortality.These threats are especially true for amphibians (frogs, newts and caecilians). Amphibians are undergoing severe population declines all over the world due to habitat destruction, climate change, disease spread, and invasive species. Losing amphibians, pest-controllers par excellence, not only imperils agricultural security but can lead to imbalances in ecosystem processes.",
+},
+
+{
+    title: "How to Take Care of a Pet Frog",
+    foto: "foto art 4.jpg",
+    code: 4,
+    category: "Amphibians",
+    date: 27-02-23,
+    views: 8947,
+    author: "Luciano Mars",
+    content: "First, it’s not so simple as just buying a frog. There are a ton of different frog species out there, and each has different needs and requirements, so you’ll need to research before you whip out your credit card. Different frogs can require completely different environments, so take everything on this list to be a guideline rather than a hard-and-fast rule. Buy a few books, ask knowledgeable breeders — do whatever you need to do to properly prepare yourself for owning your first frog. Your frog’s lifespan will depend on a variety of factors, including their species and your ability to properly care for them. On average, though, you can expect to commit 3 to 10 years to this pet, although it’s not unheard of for these animals to live much longer. Their sizes vary wildly as well, ranging from 1.5 to 5 inches. The overall size isn’t so important, as long as you know how big they’ll be once fully grown, as that will allow you to buy a tank of a suitable size.",
+},
+
+//Birds
+
+{
+    title: "Crowds flock to Beacon Hill Park as young owls learn to fly",
+    foto: "foto art 5.jpg",
+    code: 5,
+    category: "Birds",
+    date: 31-02-23,
+    views: 9487,
+    author: "Luciano Mars",
+    content: "Owlets start leaving nest around this time of year, so some people may come across young birds on the ground. Don't worry — it's a normal part of learning to fly. A family of great horned owls nesting in Beacon Hill Park is drawing crowds as the youngsters learn to fly. Photographers and birders flocked to the area this week after one of the young owls started leaving the nest to explore, known as “branching,” said Ann Nightingale, a volunteer with the Rocky Point Bird Observatory. The young owl ended up on the ground for a little while, a normal part of the process of learning to fly, she said."
+},
+
+{
+    title: "Scientists are using machine learning to forecast bird migration and identify birds in flight by their calls",
+    foto: "foto art 6.jpg",
+    code: 6,
+    category: "Birds",
+    date: 27-02-23,
+    views: 7493,
+    author: "Luciano Mars",
+    content: "With chatbots like ChatGPT making a splash, machine learning is playing an increasingly prominent role in our lives. For many of us, it’s been a mixed bag. We rejoice when our Spotify For You playlist finds us a new jam, but groan as we scroll through a slew of targeted ads on our Instagram feeds. Machine learning is also changing many fields that may seem surprising. One example is my discipline, ornithology – the study of birds. It isn’t just solving some of the biggest challenges associated with studying bird migration; more broadly, machine learning is expanding the ways in which people engage with birds. As spring migration picks up, here’s a look at how machine learning is influencing ways to research birds and, ultimately, to protect them. The challenge of conserving migratory birds. Most birds in the Western Hemisphere migrate twice a year, flying over entire continents between their breeding and nonbreeding grounds. While these journeys are awe-inspiring, they expose birds to many hazards en route, including extreme weather, food shortages and light pollution that can attract birds and cause them to collide with buildings. Our ability to protect migratory birds is only as good as the science that tells us where they go. And that science has come a long way.",
+},
+
+{
+    title: "Three of six escaped birds returned to Oakland zoo",
+    foto: "foto art 7.jpg",
+    code: 7,
+    category: "Birds",
+    date: 28-02-23,
+    views: 1683,
+    author: "Athanasia Lyric",
+    content: "Three birds have been recovered and three others are still flying free at the Oakland Zoo after a tree fell onto an aviary there amid stormy weather earlier this week, a zoo spokesperson said Friday. Two pied crows, three superb starlings and a hooded vulture flew out of the aviary in the zoo's African Savanna area after the tree fell and tore the mesh on the structure during wet and windy conditions Tuesday. The three superb starlings, a small and brightly colored bird native to eastern Africa, flew back into their habitat and were recovered Thursday, but zoo staff still are trying to recover the crows and vulture, zoo spokesperson Isabella Linares said Friday. The three birds <q> are close by and on zoo grounds,</q> Linares said. Staff have eyes on the three birds still on the loose and are monitoring them 24 hours a day, she said. A pathway in the African Savanna area of the zoo where the tree fell had to close Thursday, so visitors that day were not able to see the African elephants further along that path, Linares said.",
+},
+
+{
+    title: "Audubon annual backyard survey: Fewer birds seen",
+    foto: "foto art 8.jpg",
+    code: 8,
+    category: "Birds",
+    date: 28-02-23,
+    views: 2849,
+    author: "Silvestra Mariana",
+    content: "Early returns from New Hampshire Audubon’s annual Backyard Winter Bird Survey point to fewer birds at feeders this winter. Why? It turns out the answer is not simple. According to Dr. Pamela Hunt of N.H. Audubon, “There are many factors that influence the number of birds a person might see at their feeders during the winter, and most of them are not indicative of long-term population trends.” In winter, birds form feeding flocks and wander around in search of food. If food is plentiful they may not need to visit feeders except in bad weather. If food is scarce, many birds might leave entirely. Food means natural food crops called mast, the seeds from trees, weeds and fruit. Blue jays, for example, depend on acorns for food, and in the fall of 2022, they left the state because of a poor acorn crop. Other winter visitors, such as the familiar dark-eyed junco (or “snowbird”), migrate a variable distance depending on food availability. If food is scarce in the North, more will continue south to the mid-Atlantic, but they’ll stay put if there’s plenty to eat in northern New England. Then there are the birds called “winter finches,” including evening grosbeak and common redpoll. In most years, these birds are quite rare, but they can invade in huge numbers when their normal foods are hard to find in Canada. Even the familiar American goldfinch shows this pattern, although they are still present each winter.",
+},
+
+//Fish
+
+{
+    title: "Rockfish Genes Hold Clues To Human Longevity",
+    foto: "foto art 9.jpg",
+    code: 9,
+    category: "Fish",
+    date: 31-02-23,
+    views: 8475,
+    author: "Athanasia Lyric",
+    content: "lorem ipsum",
+},
+
+{
+    title: "A Fish Can Sense another’s Fear, A Study Shows",
+    foto: "foto art 10.jpg",
+    code: 10,
+    category: "Fish",
+    date: 29-02-23,
+    views: 9375,
+    author: "Luciano Mars",
+    content: "lorem ipsum",
+},
+
+{
+    title: "Ancient Monster Fish “Dunk” Was Short and Chunky, Study Finds",
+    foto: "foto art 11.jpg",
+    code: 11,
+    category: "Fish",
+    date: 27-02-23,
+    views: 8379,
+    author: "Athanasia Lyric",
+    content: "lorem ipsum",
+},
+
+{
+    title: "Dolphin and Humans Work Together To Catch Fish In Brazil",
+    foto: "foto art 12.jpg",
+    code: 12,
+    category: "Fish",
+    date: 27-02-23,
+    views: 2937,
+    author: "Natanaele Bharat Elyj",
+    content: "lorem ipsum",
+},
+
+//Mammals
+
+{
+    title: "World's largest rodents are taking over a fancy gated community in Argentina",
+    foto: "foto art 13.jpg",
+    code: 13,
+    category: "Mammals",
+    date: 31-02-23,
+    views: 10453,
+    author: "Silvestra Mariana",
+    content: "lorem ipsum",
+},
+
+{
+    title: "Woman Finds Clump Of Baby Squirrels In Her Yard And Helps Reunite Them With Mom",
+    foto: "foto art 14.jpg",
+    code: 14,
+    category: "Mammals",
+    date: 29-02-23,
+    views: 8475,
+    author: "Athanasia Lyric",
+    content: "lorem ipsum",
+},
+
+{
+    title: "Kitten Sneaks Out Of Crate To Comfort Scared Dog At The Vet",
+    foto: "foto art 15.jpg",
+    code: 15,
+    category: "Mammals",
+    date: 28-02-23,
+    views: 8217,
+    author: "Natanaele Bharat Elyj",
+    content: "lorem ipsum",
+},
+
+{
+    title: "Hopeless Stray Dog Has The Sweetest Reaction When She Finally Finds Love",
+    foto: "foto art 16.jpg",
+    code: 16,
+    category: "Mammals",
+    date: 27-02-23,
+    views: 1743,
+    author: "Silvestra Mariana",
+    content: "lorem ipsum",
+},
+
+
+//Reptiles
+
+{
+    title: "What You Should Know About Pet Geckos",
+    foto: "foto art 17.jpg",
+    code: 17,
+    category: "Reptiles",
+    date: 31-02-23,
+    views: 4345,
+    author: "Natanaele Bharat Elyj",
+    content: "lorem ipsum",
+},
+
+
+{
+    title: "How to Play With a Pet Lizard",
+    foto: "foto art 18.jpg",
+    code: 18,
+    category: "Reptiles",
+    date: 29-02-23,
+    views: 2723,
+    author: "Natanaele Bharat Elyj",
+    content: "lorem ipsum",
+},
+
+
+{
+    title: "Are turtles reptiles or amphibians? Here’s what you need to know",
+    foto: "foto art 19.jpg",
+    code: 19,
+    category: "Reptiles",
+    date: 28-02-23,
+    views: 3847,
+    author: "Athanasia Lyric",
+    content: "lorem ipsum",
+},
+
+
+{
+    title: "How To Find (And Adopt) The Best Pet Snake For You",
+    foto: "foto art 20.jpg",
+    code: 20,
+    category: "Reptiles",
+    date: 27-02-23,
+    views: 2847,
+    author: "Athanasia Lyric",
+    content: "lorem ipsum",
+},
+
+
+];
+
+
+
+
+
+
+
+var reviews = [
+{
+    name: "",
+    //name->nama produk
+    product: "",
+    //product -> foto produk
+    star: "",
+    description: "",
+    comment: "",
+    //comment -> review pengguna
+},
+
+{
+    name: "",
+    product: "",
+    star: "",
+    description: "",
+    comment: "",
+},
+
+]
